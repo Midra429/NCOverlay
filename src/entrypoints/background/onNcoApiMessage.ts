6d9ba7c1bf3056ec631c22ae80f66fa146da790d
Midra <me@midra.me>
2024-07-18 17:33:17 +0900

    こっちにも全部追加

--- src/entrypoints/background/onNcoApiMessage.ts
+++ src/entrypoints/background/onNcoApiMessage.ts
@@ -23,11 +23,31 @@ export default () => {
     return ncoApi.niconico.threads(...data)
   })
 
+  ncoApiMessenger.onMessage('jikkyo.kakolog', ({ data }) => {
+    return ncoApi.jikkyo.kakolog(...data)
+  })
+
   ncoApiMessenger.onMessage('syobocal.json', ({ data }) => {
     return ncoApi.syobocal.json(...data)
   })
 
-  ncoApiMessenger.onMessage('jikkyo.kakolog', ({ data }) => {
-    return ncoApi.jikkyo.kakolog(...data)
+  ncoApiMessenger.onMessage('danime.part', ({ data }) => {
+    return ncoApi.danime.part(...data)
+  })
+
+  ncoApiMessenger.onMessage('abema.program', ({ data }) => {
+    return ncoApi.abema.program(...data)
+  })
+
+  ncoApiMessenger.onMessage('dmmTv.video', ({ data }) => {
+    return ncoApi.dmmTv.video(...data)
+  })
+
+  ncoApiMessenger.onMessage('fod.episode', ({ data }) => {
+    return ncoApi.fod.episode(...data)
+  })
+
+  ncoApiMessenger.onMessage('unext.title', ({ data }) => {
+    return ncoApi.unext.title(...data)
   })
 }
