a56ae543a7c3b21bb4876f9e08cdc634017fafdf
Midra <me@midra.me>
2024-07-19 22:29:13 +0900

    動画要素のセレクタを変更

--- src/entrypoints/vod-abema.content/index.ts
+++ src/entrypoints/vod-abema.content/index.ts
@@ -84,7 +84,7 @@ const main = async () => {
     } else if (!patcher.nco) {
       if (location.pathname.startsWith('/video/episode/')) {
         const video = document.body.querySelector<HTMLVideoElement>(
-          '.com-a-Video__video > video[preload="metadata"][src]'
+          '.com-a-Video__video > video[preload][src]'
         )
 
         if (video) {
