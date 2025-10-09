e1353bc7e96534c5be6a84ff0301b6e71c794e2c
Midra <me@midra.me>
2025-10-09 17:00:42 +0900

    descriptionのレイアウトを修正

--- src/components/SettingsInput/Select.tsx
+++ src/components/SettingsInput/Select.tsx
@@ -1,6 +1,8 @@
 import type { StorageItems, SettingsKey } from '@/types/storage'
 import type { SettingsInputBaseProps } from '.'
 
+import { cn } from '@heroui/react'
+
 import { useSettings } from '@/hooks/useSettings'
 
 import { Select, SelectItem } from '@/components/Select'
@@ -27,32 +29,46 @@ export function Input(props: Props) {
   const [value, setValue] = useSettings(props.settingsKey)
 
   return (
-    <Select
-      classNames={{
-        base: 'py-2',
-        mainWrapper: 'w-32',
-      }}
-      size="sm"
-      label={props.label}
-      labelPlacement="outside-left"
-      description={props.description}
-      selectedKeys={[JSON.stringify(value)]}
-      onSelectionChange={([key]) => setValue(key && JSON.parse(key as string))}
-      renderValue={([{ props, rendered }]) => (
-        <>
-          {props?.startContent}
-          <span>{rendered}</span>
-        </>
-      )}
-    >
-      {props.options.map(({ label, value, Icon }) => (
-        <SelectItem
-          key={JSON.stringify(value)}
-          startContent={Icon && <Icon className="size-4" />}
+    <div className="flex flex-col">
+      <Select
+        classNames={{
+          base: 'py-2',
+          mainWrapper: 'w-32',
+        }}
+        size="sm"
+        label={props.label}
+        labelPlacement="outside-left"
+        selectedKeys={[JSON.stringify(value)]}
+        onSelectionChange={([key]) =>
+          setValue(key && JSON.parse(key as string))
+        }
+        renderValue={([{ props, rendered }]) => (
+          <>
+            {props?.startContent}
+            <span>{rendered}</span>
+          </>
+        )}
+      >
+        {props.options.map(({ label, value, Icon }) => (
+          <SelectItem
+            key={JSON.stringify(value)}
+            startContent={Icon && <Icon className="size-4" />}
+          >
+            {label}
+          </SelectItem>
+        ))}
+      </Select>
+
+      {props.description && (
+        <span
+          className={cn(
+            'text-tiny mb-2 whitespace-pre-wrap',
+            'text-foreground-500 dark:text-foreground-600'
+          )}
         >
-          {label}
-        </SelectItem>
-      ))}
-    </Select>
+          {props.description}
+        </span>
+      )}
+    </div>
   )
 }
