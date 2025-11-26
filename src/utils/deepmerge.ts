e7229031de744fe5cc41abed3b9b8c5d5a1c42ad
Midra <me@midra.me>
2025-11-26 23:19:29 +0900

    片方がundefinedのときのエラー修正

--- src/utils/deepmerge.ts
+++ src/utils/deepmerge.ts
@@ -1,15 +1,19 @@
 import merge from 'deepmerge'
 
-export function deepmerge<T1, T2>(x: T1, y: T2): T1 & T2 {
-  return merge(x as any, y as any, {
-    customMerge: (_, options) => {
-      return (x, y) => {
-        if (typeof y === 'undefined') {
-          return x
-        }
-
-        return merge(x, y, options)
+const options: merge.Options = {
+  customMerge: (_, options) => {
+    return (x, y) => {
+      if (typeof x === 'undefined') {
+        return y
+      } else if (typeof y === 'undefined') {
+        return x
       }
-    },
-  })
+
+      return merge(x, y, options)
+    }
+  },
+}
+
+export function deepmerge<T1, T2>(x: T1, y: T2): T1 & T2 {
+  return merge(x ?? {}, y ?? {}, options) as any
 }
