8941d6caeb6a1664286f63fcc631e3f037ee0277
Midra <me@midra.me>
2025-12-06 00:12:33 +0900

    設定移行後に再起動

--- src/entrypoints/background/migration.ts
+++ src/entrypoints/background/migration.ts
@@ -1,22 +1,25 @@
 import type { StorageItems_v4 } from '@/types/storage'
 
 import { logger } from '@/utils/logger'
+import { webext } from '@/utils/webext'
 import { storage } from '@/utils/storage/extension'
 
 export default async function () {
-  let migrate_version = (await storage.get('_migrate_version')) ?? 1
+  const _migrate_version = await storage.get('_migrate_version')
+
+  let version = _migrate_version ?? 1
 
   // v3.13.4 -> v3.20.2
-  if (migrate_version < 4) {
+  if (version < 4) {
     logger.log('migration', '3 -> 4')
 
     // @ts-ignore
     await storage.remove('settings:ng:useNiconicoAccount')
 
-    migrate_version = 4
+    version = 4
   }
 
-  if (migrate_version < 5) {
+  if (version < 5) {
     logger.log('migration', '4 -> 5')
 
     const [autoLoads, jikkyoChannelIds] = (await storage.get(
@@ -37,8 +40,12 @@ export default async function () {
     // @ts-ignore
     await storage.remove('settings:comment:jikkyoChannelIds')
 
-    migrate_version = 5
+    version = 5
   }
 
-  await storage.set('_migrate_version', migrate_version)
+  if (version !== _migrate_version) {
+    await storage.set('_migrate_version', version)
+
+    webext.runtime.reload()
+  }
 }
