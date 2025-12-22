a6e7708fe3d570412eb6381e191477a3d13caf07
Midra <me@midra.me>
2025-12-23 07:49:35 +0900

    `.0`まで必要らしい

--- wxt.config.ts
+++ wxt.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
         browser_specific_settings = {
           gecko: {
             id: `${name}@midra.me`,
-            strict_min_version: '142',
+            strict_min_version: '142.0',
             // @ts-expect-error
             data_collection_permissions: {
               required: ['none'],
